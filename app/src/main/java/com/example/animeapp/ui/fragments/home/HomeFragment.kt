528fa7c25801f0eb137c3b4c6530a517482336b6
JackaBolotbekov <111832311+JackaBolotbekov@users.noreply.github.com>
2023-02-08 14:50:36 +0600

    IspravilOshibki

--- app/src/main/java/com/example/animeapp/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/animeapp/ui/fragments/home/HomeFragment.kt
@@ -1,20 +1,20 @@
 package com.example.animeapp.ui.fragments.home
 
-import androidx.fragment.app.viewModels
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.animeapp.R
-import com.example.animeapp.base.BaseFragment
-import com.example.animeapp.base.BaseViewModel
 import com.example.animeapp.databinding.FragmentHomeBinding
 import com.example.animeapp.ui.adapters.PagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
-class HomeFragment : BaseFragment<FragmentHomeBinding, BaseViewModel>(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    override val binding by viewBinding(FragmentHomeBinding::bind)
-    override val viewModel by viewModels<BaseViewModel>()
+    private val binding by viewBinding(FragmentHomeBinding::bind)
 
-    override fun initialize() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         tabLayout()
     }
 
