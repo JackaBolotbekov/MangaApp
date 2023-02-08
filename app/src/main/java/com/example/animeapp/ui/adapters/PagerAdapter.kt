cde702b5ea602af492cd68577d0360efe9bd712f
JackaBolotbekov <111832311+JackaBolotbekov@users.noreply.github.com>
2023-02-08 13:27:49 +0600

    IspravilOshibki

--- app/src/main/java/com/example/animeapp/ui/adapters/PagerAdapter.kt
+++ app/src/main/java/com/example/animeapp/ui/adapters/PagerAdapter.kt
@@ -1,9 +1,6 @@
 package com.example.animeapp.ui.adapters
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.animeapp.ui.fragments.anime.AnimeFragment
 import com.example.animeapp.ui.fragments.manga.MangaFragment
